1dcf9fbba0fe6580dadf24f860f2ece3eafcdd14
Andry OELIHARIVONY <andry.oeliharivony@partenamut.be>
2023-09-17 13:17:10 +0200

    initial commit

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Product } from '../products';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent {
+   items : Product[] | undefined;
+
+   checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+   });
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder){
+
+  }
+
+  ngOnInit(){
+    this.items = this.cartService.getItems();
+  }
+
+  onSubmit() {
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+}
